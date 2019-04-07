a22defae18986204fe71ab368f25975492647679
Peyton Gardipee <peytongardipee1@Peytons-MacBook-Pro.local>
2019-04-07 14:34:39 -0500

    3 tab react project created

---
+++ AwesomeProject/navigation/MainTabNavigator.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import { Platform } from 'react-native';
+import { createStackNavigator, createBottomTabNavigator } from 'react-navigation';
+
+import TabBarIcon from '../components/TabBarIcon';
+import HomeScreen from '../screens/HomeScreen';
+import LinksScreen from '../screens/LinksScreen';
+import SettingsScreen from '../screens/SettingsScreen';
+
+const HomeStack = createStackNavigator({
+  Home: HomeScreen,
+});
+
+HomeStack.navigationOptions = {
+  tabBarLabel: 'Review',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={
+        Platform.OS === 'ios'
+          ? `ios-link${focused ? '' : '-outline'}`
+          : 'md-link'
+      }
+    />
+  ),
+};
+
+const LinksStack = createStackNavigator({
+  Links: LinksScreen,
+});
+
+LinksStack.navigationOptions = {
+  tabBarLabel: 'Ranking',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={Platform.OS === 'ios' ? 'ios-home' : 'md-home'}
+    />
+  ),
+};
+
+const SettingsStack = createStackNavigator({
+  Settings: SettingsScreen,
+});
+
+SettingsStack.navigationOptions = {
+  tabBarLabel: 'Settings',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={Platform.OS === 'ios' ? 'ios-options' : 'md-options'}
+    />
+  ),
+};
+
+export default createBottomTabNavigator({
+  HomeStack,
+  LinksStack,
+  SettingsStack,
+});
---
+++ AwesomeProject/screens/SettingsScreen.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import { Text, StyleSheet, View, TextInput, ScrollView, TouchableOpacity} from 'react-native';
+import { ExpoConfigView } from '@expo/samples';
+
+export default class SettingsScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Settings',
+  };
+
+  render() {
+    /* Go ahead and delete ExpoConfigView and replace it with your
+     * content, we just wanted to give you a quick view of your config */
+    //return <ExpoConfigView />;
+    return (
+    <ScrollView>
+          <Text>Scroll me plz</Text>
+          <Text>Scroll me plz</Text>
+          <Text>Scroll me plz</Text>
+          <Text>Scroll me plz</Text>
+          <Text>Scroll me plz</Text>
+          <Text>Scroll me plz</Text>
+          <Text>Scroll me plz</Text>
+          <Text>Scroll me plz</Text>
+          <Text>Scroll me plz</Text>
+          <Text>Scroll me plz</Text>
+          <Text>Scroll me plz</Text>
+          <Text>Scroll me plz</Text>
+          <Text>Scroll me plz</Text>
+          <Text>Scroll me plz</Text>
+          <Text>Scroll me plz</Text>
+          <Text>Scroll me plz</Text>
+          <Text>Scroll me plz</Text>
+          <Text>Scroll me plz</Text>
+          <Text>Scroll me plz</Text>
+          <Text>Scroll me plz</Text>
+          <Text>Scroll me plz</Text>
+          <Text>Scroll me plz</Text>
+          <Text>Scroll me plz</Text>
+          <Text>Scroll me plz</Text>
+          <Text>Scroll me plz</Text>
+          <Text>Scroll me plz</Text>
+          <Text>Scroll me plz</Text>
+          <Text>Scroll me plz</Text>
+          <Text>Scroll me plz</Text>
+          <Text>Scroll me plz</Text>
+          <Text>Scroll me plz</Text>
+          <Text>Scroll me plz</Text>
+          <Text>Scroll me plz</Text>
+          <Text>Scroll me plz</Text>
+          <Text>Scroll me plz</Text>
+          <Text>Scroll me plz</Text>
+          <Text>Scroll me plz</Text>
+          <Text>Scroll me plz</Text><Text>Scroll me plz</Text>
+          <Text>Scroll me plz</Text>
+          <Text>Scroll me plz</Text>
+          <Text>Scroll me plz</Text>
+          <Text>Scroll me plz</Text>
+          <Text>Scroll me plz</Text>
+          <Text>Scroll me plz</Text>
+          <Text>Scroll me plz</Text>
+          <Text>Scroll me plz</Text>
+          <Text>Scroll me plz</Text>
+          <Text>Scroll me plz</Text>
+          <Text>Scroll me plz</Text>
+          <Text>Scroll me plz</Text>
+          <Text>Scroll me plz</Text>
+          <Text>Scroll me plz</Text>
+          <Text>Scroll me plz</Text>
+          <Text>Scroll me plz</Text>
+          <Text>Scroll me plz</Text>
+          <Text>Scroll me plz</Text>
+          
+          
+    </ScrollView>
+    );
+  }
+    
+}
